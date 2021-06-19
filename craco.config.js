8728db9f91a1c9dcea54e4263374023eaadf7ff1
xuRookie <xu128523@163.com>
2021-06-19 13:31:47 +0800

    更改craco配置文件

--- craco.config.js
+++ craco.config.js
@@ -9,6 +9,27 @@ module.exports = {
       '@': resolve('src'),
       '@assets': resolve('src/assets')
     },
+    /**
+     * 重写 webpack 任意配置
+     *  - configure 能够重写 webpack 相关的所有配置，但是，仍然推荐你优先阅读 craco 提供的快捷配置，把解决不了的配置放到 configure 里解决；
+     *  - 这里选择配置为函数，与直接定义 configure 对象方式互斥；
+     */
+    configure: (webpackConfig, {
+        env, paths
+    }) => {
+        // paths.appPath='public'
+        paths.appBuild = 'build'
+        webpackConfig.output = {
+          ...webpackConfig.output,
+            // ...{
+            //   filename: whenDev(() => 'static/js/bundle.js', 'static/js/[name].js'),
+            //   chunkFilename: 'static/js/[name].js'
+            // },
+            path: path.resolve(__dirname, 'build'), // 修改输出文件目录
+            publicPath: './'
+        }
+        return webpackConfig
+    }
   },
   devServer: {
     port: 3002,
